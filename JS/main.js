029b4feb6b1c687cddb97014e469d1a21a0bcf48
Gabriel Kist Camargo <gabrielkcamargo0@hotmail.com>
2023-06-13 20:48:34 -0300

    Finalizando Exercicio

--- JS/main.js
+++ JS/main.js
@@ -1,55 +1,55 @@
 $(document).ready(function () {
-    $("#carrossel__slides").slick({
-        autoplay: true,
-    });
+  $("#Telefone").mask("(00) 00000-0000");
+  $("#CPF").mask("000.000.000-00");
+  $("#CEP").mask("00000-000");
 
-    $(".menu__hamburguer").click(function(){
-        $("nav").slideToggle();
-    })
-    
-    $("#Telefone").mask("(00) 00000-0000")
-    
-    $('form').validate({
-        rules:{
-            nome: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            telefone: {
-                required: true
-            },
-            mensagem__enviar:{
-                required: true
-            },
-            Veiculo__interesse:{
-                required: false
-            }
-        },
-        messages:{
-            nome: 'Por favor, insira seu nome'
-        },
-        submitHandler: function(form){
-            console.log(form)
-        },
-        invalidHandler: function(evento, validador){
-            let camposIncorretos = validador.numberOfInvalids()
-            if( camposIncorretos){
-                alert(`Existem ${camposIncorretos} campos incorretor`)
-            }
-        }
-    })
-    $('.lista__veiculos button').click(function(){
-        const destino = $("#Contato")
-
-        const nomeVeiculo = $(this).parent().find("h3").text()
-
-        $("#Veiculo__interesse").val(nomeVeiculo)
-
-        $("html").animate({
-            scrollTop: destino.offset().top
-        }, 1000)
-    })
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+      },
+      CPF: {
+        required: true,
+      },
+      endereço: {
+        required: true,
+      },
+      CEP: {
+        required: true,
+      },
+    },
+    messages: {
+      nome: "*Por favor, insira seu nome",
+    },
+    submitHandler: function (form) {
+      console.log(form);
+    },
+    invalidHandler: function (evento, validador) {
+      let camposIncorretos = validador.numberOfInvalids();
+      if (camposIncorretos) {
+        $(".Mensagem__confirmação").text(
+          `Existem ${camposIncorretos} campos incorretos`
+        );
+        $(".Mensagem__confirmação").css("display", "block");
+        $(".Mensagem__confirmação").css(
+          "background-color",
+          "var(--cor-error)"
+        );
+      }
+    },
+    submitHandler: function (form) {
+      if ($("form").valid()) {
+        $(".Mensagem__confirmação").text("Cadastro concluído");
+        $(".Mensagem__confirmação").css("display", "block");
+      }
+      console.log(form);
+    },
   });
+});
